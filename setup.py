9e9dc5a248dc3838b4b6dd5e691df7a3fac335cb
Arcoson <gupn8637@lgsstudent.org>
2025-08-09 14:13:56 -0700

    0.5.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="gradientvis",  # The name of the package
-    version="0.1.0",  # The version number
+    version="0.5.0",  # The version number
     description="A library for visualizing neural network gradients",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="XCALEN",
-    author_email="hylendust@gmail.com",
-    url="https://github.com/Arcoson/gradientvis",
+    author_email="desenyon@gmail.com",
+    url="https://github.com/desenyon/gradientvis",
     packages=find_packages(),  # Automatically find packages
     install_requires=[
         "torch",  # List any required dependencies
